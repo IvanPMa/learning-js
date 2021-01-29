72e2bdafbb032055568afdc5a9fb619cb0a10cde
IvanPM <pachecomaldonadoivan@gmail.com>
2021-01-29 15:31:23 -0600

    Manejo de arreglos

--- Bloque 2/18 Arrays/js/arraays.js
+++ Bloque 2/18 Arrays/js/arraays.js
@@ -2,7 +2,23 @@
 
 //Arrays,arreglos,matrices
 var nombres =["Ivan","Pacheco","Maldonado",true,52];
-console.log(nombres);
-var lenguajes = new Array("php","js","java","c++");
-console.log(lenguajes);
-console.log(nombres[2]);
+// console.log(nombres);
+var lenguajes = new Array("php","js","java","c++","ruby","c#");
+// console.log(lenguajes);
+// var ubicacion= parseInt(prompt("Ubicacion en el Array",0));
+// console.log(nombres.length);
+
+
+// if(ubicacion < nombres.length && ubicacion>= 0){
+//     console.log(nombres[ubicacion]);
+// }
+// else{
+//     console.log("Ingresa un indice correcto");
+
+// }
+document.write("Listado de lenguajes");
+document.write("<ul>");
+for(var i =0; i<lenguajes.length; i++){
+    document.write("<li>"+lenguajes[i]+"</li>");
+}
+document.write("</ul>");
