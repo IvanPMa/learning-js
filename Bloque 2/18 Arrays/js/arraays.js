f383c18d50608a84960c8d98c61448d03205cbb9
IvanPM <pachecomaldonadoivan@gmail.com>
2021-01-29 15:14:11 -0600

    Sintaxis de un array

---
+++ Bloque 2/18 Arrays/js/arraays.js
@@ -0,0 +1,8 @@
+'use strict'
+
+//Arrays,arreglos,matrices
+var nombres =["Ivan","Pacheco","Maldonado",true,52];
+console.log(nombres);
+var lenguajes = new Array("php","js","java","c++");
+console.log(lenguajes);
+console.log(nombres[2]);
