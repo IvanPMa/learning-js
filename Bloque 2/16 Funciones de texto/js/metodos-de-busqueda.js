fdce0d3a99724c0c4116f3eb19d77241d89b556f
IvanPM <pachecomaldonadoivan@gmail.com>
2021-01-29 14:19:07 -0600

    Funciones de busqueda basicas

--- Bloque 2/16 Funciones de texto/js/metodos-de-busqueda.js
+++ Bloque 2/16 Funciones de texto/js/metodos-de-busqueda.js
@@ -11,5 +11,15 @@ var busqueda2 = texto1.lastIndexOf("prueba");
 var busqueda3 = texto1.match("prueba");
 var busqueda3 = texto1.match(/prueba/g);
 var busqueda4 = texto1.substr(13,6);
+var busqueda5 = texto1.charAt(15);
+//starts with busca al inicio del String la palabra
+var busqueda6 = texto1.startsWith("primer");
+var busqueda7 = texto1.endsWith("segunda vez de prueba");
+//buscar la palabra 
+var busqueda8 = texto1.includes("segunda");
 console.log(typeof busqueda3);
 console.log(busqueda4);
+console.log(busqueda5);
+console.log(busqueda6);
+console.log(busqueda7);
+console.log("Esta la palabara: ",busqueda7);
