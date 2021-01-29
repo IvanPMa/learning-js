8d4bd7d35fd214398c3f1d6a3eb4bc9b2bf16c15
IvanPM <pachecomaldonadoivan@gmail.com>
2021-01-28 18:40:40 -0600

    Funciones de texto principales

--- Bloque 2/16 Funciones de texto/js/funciones-texto.js
+++ Bloque 2/16 Funciones de texto/js/funciones-texto.js
@@ -1,1 +1,30 @@
 'use strict'
+
+//Transformacion de textos
+
+var numero = 44;
+console.log(typeof numero);
+var texto1 = "esto es una variable";
+var texto2 = "Segundo texto";
+
+var dato = numero.toString();
+dato = texto1.toUpperCase();
+console.log(dato);
+dato = texto2.toLowerCase();
+console.log(dato);
+
+//Calcular longitud de un texto
+//Cuando es undefined es por que no es texto
+var nombre = numero.toString();
+console.log(nombre.length);
+
+//Array
+
+var nombre = ["hola","como","estas"];
+console.log(nombre.length);
+
+//Concatenar
+// var textoTotal = texto1+texto2+numero;
+
+var textoTotal = texto1.concat(" "+texto2+" "+numero);
+console.log(textoTotal);
